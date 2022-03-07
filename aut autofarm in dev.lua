55b58e4df53d349e1f59009ad9a55888ca0bf784
Kranzyo <100553582+Kranzyo@users.noreply.github.com>
2022-03-07 00:05:02 -0500

    added functions for cleaner code

--- aut autofarm in dev.lua
+++ aut autofarm in dev.lua
@@ -22,24 +22,36 @@ local function fireproximityprompt(Obj, Amount, Skip)
         error("userdata<ProximityPrompt> expected")
     end
 end
+getgenv.Config = {
 
-local itemFarms = {
-    "Meteor Farm",
-    "Chest Farm",
-    "Sand Debris Farm",
-    "Item Farm"
+    itemFarms = {
+        "Meteor Farm",
+        "Chest Farm",
+        "Sand Debris Farm",
+        "Item Farm"
+    },
+
+    plr = {
+        Speed = 16
+        Jump = 50
+    },
+
+    farmMethod = "Meteor Farm",
+    OnFarm = false,
+
+    desc = [[
+        
+    A Universal time autofarm
+    **USE AT YOUR OWN RISK**]]
 }
+
+
 
 local lp = game:GetService("Players").LocalPlayer
 local char = lp.Character
-local hum = char.Humanoid
-local root = char:FindFirstChild("HumanoidRootPart")
+local hum = char:WaitForChild("Humanoid")
+local root = char:WaitForChild("HumanoidRootPart")
  
-local desc = [[
-
-    A Universal time autofarm
-    **USE AT YOUR OWN RISK**
-]]
 local EnumKeys = {'Semicolon','Tab','Equals','Comma','Minus','Period','F1',"F2","F3","F4",'F5',"F6","F7",
     "F8","F9","F10","F11","F12",'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H',
     'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M','Slash','One','Two','Three',"Four","Five","Six","Seven","Eight",
@@ -64,37 +76,54 @@ local scriptCreator = CreditsCategory:Sector("Script Creator:")
 local description = CreditsCategory:Sector("Description:")
 uiCreator:Cheat("Label", "Detourious @ v3rmillion.net") 
 scriptCreator:Cheat("Label", "KayD @ v3rmillion.net")
-description:Cheat("Label", desc)
+description:Cheat("Label", Config.desc)
 local Notes = CreditsCategory:Sector("NOTE:")
-Notes:Cheat("Label", "Re execute if anything it doesn't work")
+Notes:Cheat("Label", "Re execute if anything doesn't work")
+
 -- // main
 local autofarm = FinityWindow:Category("Autofarm")
 local farms = autofarm:Sector("Farms")
 -- // Dropdown to select method
 farms:Cheat("Dropdown", "Select Farm Method", 
 function(s)
-    getgenv().farmMethod = s
+    Config.farmMethod = s
 end,
 {
-options = itemFarms
+options = Config.itemFarms
 })
 -- // farm
 farms:Cheat("Checkbox", "Farm Selected", 
 function(v)
-    getgenv().OnFarm = v
+    Config.OnFarm = v
 
-    -- // tp
-    while OnFarm do
+    if OnFarm then
+        if Config.farmMethod == "Meteor Farm" then
+            meteorFarm()
+        elseif Config.farmMethod == "Chest Farm" then
+            chestFarm()
+        elseif Config.farmMethod == "Sand Debris Farm" then
+            sandDebrisFarm()
+        elseif Config.farmMethod == "Item Farm" then
+            itemFarm()
+        end
+    end
+end)
+
+--[[ old code
 
         -- // Meteor farm
+        
         if farmMethod == "Meteor Farm" then
+            print("Meteor farming...")
             for i,v in pairs(game:GetService("Workspace").ItemSpawns.Meteors:GetDescendants()) do
-                if v.Name == "Meteor" then
-                    root.CFrame = v.CFrame + Vector3.new(0,7,0)
+                if lp.Character then
+                    if v.Name == "Meteor" then
+                        root.CFrame = v.CFrame + Vector3.new(0,7,0)
+                    end
                 end
             end
 
-            -- // collection
+                -- // collection
             for i,v in pairs(game:GetService("Workspace").ItemSpawns.Meteors:GetDescendants()) do
                 if v:FindFirstChild("Interaction") then
                     fireproximityprompt(v.Interaction, 1, true)
@@ -103,15 +132,16 @@ function(v)
 
                 repeat task.wait() until OnFarm
 
-            -- // chest farm
+        -- // chest farm
         elseif farmMethod == "Chest Farm" then
+            print("Chest Farming...")
             for i,v in pairs(game:GetService("Workspace").ItemSpawns.Chests:GetDescendants()) do
                 if v.Name == "RootPart" then
                     root.CFrame = v.CFrame + Vector3.new(0,7,0)
                 end
             end
 
-            -- // collection
+                -- // collection
             for i,v in pairs(game:GetService("Workspace").ItemSpawns.Chests:GetDescendants()) do
                 if v:FindFirstChild("Interaction") then
                     fireproximityprompt(v.Interaction, 1, true)
@@ -121,6 +151,7 @@ function(v)
                 repeat task.wait() until OnFarm
 
         elseif farmMethod == "Sand Debris Farm" then
+            print("Sand farming...")
             for i,v in pairs(game:GetService("Workspace").ItemSpawns["Sand Debris"]:GetDescendants()) do
                 if v.Name == "SandDebris" then
                     root.CFrame = v.CFrame + Vector3.new(0,0,5)
@@ -134,8 +165,9 @@ function(v)
             end
 
                 repeat task.wait() until OnFarm
-            
+                
         elseif farmMethod == "Item Farm" then
+            print("Regular farm....")
             for i,v in pairs(game:GetService("Workspace").ItemSpawns.StandardItems:GetDescendants()) do
                 if v.Name == "SpawnLocation" then
                     if #v:GetChildren() > 0 then
@@ -143,25 +175,23 @@ function(v)
                     end
                 end
             end
-        
---[[ previous stuff
-    for i,v in pairs(game:GetService("Workspace").ItemSpawns.StandardItems:GetDescendants()) do
-                if v.Name == "SpawnLocation" then
-                    root.CFrame = v.CFrame
-                end
-            end
-]]
+                    
             for i,v in pairs(game:GetService("Workspace").ItemSpawns.StandardItems:GetDescendants()) do
                 if v:FindFirstChild("Interaction") then
                     fireproximityprompt(v.Interaction, 1, true)
                 end
             end
-
-            
                 repeat task.wait() until OnFarm
-        end
-    end
-end)
+                
+        end]]
+--[[ previous stuff
+
+                        for i,v in pairs(game:GetService("Workspace").ItemSpawns.StandardItems:GetDescendants()) do
+                                    if v.Name == "SpawnLocation" then
+                                        root.CFrame = v.CFrame
+                                    end
+                                end
+                    ]]
 
 -- // Misc
 local misc = FinityWindow:Category("Misc")
@@ -170,21 +200,23 @@ local godMode = misc:Sector("God Mode")
 
 -- // Walk speed and Jump power changer
 SpeednJump:Cheat("Slider", "Walk Speed", function(s)
-    getgenv().Speed = s
+    Config.plr.Speed = s
 end, {min = 16, max = 250, suffix = " Walk Speed"})
 SpeednJump:Cheat("Slider", "Jump Power", function(s)
-    getgenv().Jump = s
+    Config.plr.Jump = s
 end, {min = 50, max = 250, suffix = " Jump Power"})
 SpeednJump:Cheat("Checkbox", "Start Walk/jump power", function(s)
-    yes = s
+yes = s
     while yes do task.wait()
-        hum.WalkSpeed = Speed
-        hum.JumpPower = Jump
+        if lp.Character then
+            hum.WalkSpeed = Config.plr.Speed
+            hum.JumpPower = Config.plr.Jump
+        end   
     end
 end)
 
 -- // god mode
-godMode:Cheat("Button", "God Mode button", function()
+godMode:Cheat("Button", "God Mode", function()
     game:GetService("Players").LocalPlayer.Character.Values.Block:Destroy()
 end)
 
@@ -205,6 +237,102 @@ local deletegui = Settings:Sector("Deletegui")
 deletegui:Cheat("Button", "Delete GUI", function()
     game.CoreGui.FinityUI:Destroy()
 end)
+
+-- // functions
+local function meteorFarm()
+    task.spawn(function()
+        while task.wait() do
+            if farmMethod == "Meteor Farm" then
+                if lp.Character then
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns.Meteors:GetDescendants()) do
+                            if v.Name == "Meteor" then
+                                root.CFrame = v.CFrame + Vector3.new(0,7,0)
+                            end
+                        end
+                    end
+        
+                        -- // collection
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns.Meteors:GetDescendants()) do
+                        if v:FindFirstChild("Interaction") then
+                            fireproximityprompt(v.Interaction, 1, true)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local function chestFarm()
+    task.spawn(function()
+        while task.wait() do
+            if farmMethod == "Chest Farm" then
+                if lp.Character then
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns.Chests:GetDescendants()) do
+                        if v.Name == "RootPart" then
+                            root.CFrame = v.CFrame + Vector3.new(0,7,0)
+                        end
+                    end
+
+                        -- // collection
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns.Chests:GetDescendants()) do
+                        if v:FindFirstChild("Interaction") then
+                            fireproximityprompt(v.Interaction, 1, true)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local function sandDebrisFarm()
+    task.spawn(function()
+        while task.wait() do
+            if farmMethod == "Sand Debris Farm" then
+                if lp.Character then
+                    print("Sand farming...")
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns["Sand Debris"]:GetDescendants()) do
+                        if v.Name == "SandDebris" then
+                            root.CFrame = v.CFrame + Vector3.new(0,0,5)
+                        end
+                    end
+            
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns["Sand Debris"]:GetDescendants()) do
+                        if v:FindFirstChild("Interaction") then
+                            fireproximityprompt(v.Interaction, 1, true)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local function itemFarm()
+    task.spawn(function()
+        while task.wait() do
+            if farmMethod == "Item Farm" then
+                if lp.Character then
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns.StandardItems:GetDescendants()) do
+                        if v.Name == "SpawnLocation" then
+                            if #v:GetChildren() > 0 then
+                                root.CFrame = v:GetChildren()[1].CFrame
+                            end
+                        end
+                    end
+                            
+                    for i,v in pairs(game:GetService("Workspace").ItemSpawns.StandardItems:GetDescendants()) do
+                        if v:FindFirstChild("Interaction") then
+                            fireproximityprompt(v.Interaction, 1, true)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
 
 -- // Examples?
 
